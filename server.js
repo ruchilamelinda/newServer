94aadcb960b5c7e9892be3d6876f84db8eb12674
ruchilamelinda <145304095+ruchilamelinda@users.noreply.github.com>
2024-12-23 11:08:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.use('/api/penyewaan', penyewaanRoutes);
 const ulasanRoutes = require('./routes/ulasan.route');
 app.use('/api/ulasan', ulasanRoutes);
 
+//api history
+const rentalHistoryRoutes = require('./routes/rentalHistory');
+app.use('/api/rental', rentalHistoryRoutes);
+
 // Sinkronisasi Database
 sequelize.sync(false)
   .then(() => {
